6965a0d715dcd1c79987c7a9649914444eb3c2d2
caseyfee <caseyfee3@gmail.com>
2023-01-30 10:19:32 -0800

    moved getitems out of function and fixed typo

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,7 @@
   // useful when saving the description in local storage?
   
   
-  var saveBtnEl = $("#saveBtn");
+  var saveBtnEl = $(".saveBtn");
 
   saveBtnEl.on('click', function () {
       var time = $(this).parent().attr("id");
@@ -24,27 +24,27 @@
   // attribute of each time-block be used to do this?
   //
       localStorage.setItem(time, textInput);
-      $('#hour-6 .description').val(localStorage.getItem("hour-6"));
-      $('#hour-7 .description').val(localStorage.getItem("hour-7"));
-      $('#hour-8 .description').val(localStorage.getItem("hour-8"));
-      $('#hour-9 .description').val(localStorage.getItem("hour-9"));
-      $('#hour-10 .description').val(localStorage.getItem("hour-10"));
-      $('#hour-11 .description').val(localStorage.getItem("hour-11"));
-      $('#hour-12 .description').val(localStorage.getItem("hour-12"));
-      $('#hour-13 .description').val(localStorage.getItem("hour-13"));
-      $('#hour-14 .description').val(localStorage.getItem("hour-14"));
-      $('#hour-15 .description').val(localStorage.getItem("hour-15"));
-      $('#hour-16 .description').val(localStorage.getItem("hour-16"));
-      $('#hour-17 .description').val(localStorage.getItem("hour-17"));
-      $('#hour-18 .description').val(localStorage.getItem("hour-18"));
-      $('#hour-19 .description').val(localStorage.getItem("hour-19"));
-      $('#hour-20 .description').val(localStorage.getItem("hour-20"));
-      $('#hour-21 .description').val(localStorage.getItem("hour-21"));
-      $('#hour-22 .description').val(localStorage.getItem("hour-22"));
-      $('#hour-23 .description').val(localStorage.getItem("hour-23"));
+      
 
     });
-
+    $('#hour-6 .description').val(localStorage.getItem("hour-6"));
+    $('#hour-7 .description').val(localStorage.getItem("hour-7"));
+    $('#hour-8 .description').val(localStorage.getItem("hour-8"));
+    $('#hour-9 .description').val(localStorage.getItem("hour-9"));
+    $('#hour-10 .description').val(localStorage.getItem("hour-10"));
+    $('#hour-11 .description').val(localStorage.getItem("hour-11"));
+    $('#hour-12 .description').val(localStorage.getItem("hour-12"));
+    $('#hour-13 .description').val(localStorage.getItem("hour-13"));
+    $('#hour-14 .description').val(localStorage.getItem("hour-14"));
+    $('#hour-15 .description').val(localStorage.getItem("hour-15"));
+    $('#hour-16 .description').val(localStorage.getItem("hour-16"));
+    $('#hour-17 .description').val(localStorage.getItem("hour-17"));
+    $('#hour-18 .description').val(localStorage.getItem("hour-18"));
+    $('#hour-19 .description').val(localStorage.getItem("hour-19"));
+    $('#hour-20 .description').val(localStorage.getItem("hour-20"));
+    $('#hour-21 .description').val(localStorage.getItem("hour-21"));
+    $('#hour-22 .description').val(localStorage.getItem("hour-22"));
+    $('#hour-23 .description').val(localStorage.getItem("hour-23"));
 
 
   // TODO: Add code to apply the past, present, or future class to each time
